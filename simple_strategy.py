d59dfccbe787c04376e7a9ba585586edfade2676
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2020-11-08 12:43:35 +0100

    a*

--- simple_strategy.py
+++ simple_strategy.py
@@ -13,7 +13,31 @@ class Simple(Strategy):
 
 
     def init(self):
-        self.map = []
+#         self.map = [
+#     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'X', 'X', 'H', 'O', 'W', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'X', 'H', 'O', 'C', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'H', 'O', 'O', 'O', 'O', 'O', 'W', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'H', 'O', 'O', 'O', 'O', 'C', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'O', 'C', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+#     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+# ]
+        self.map = [
+            ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'O', 'X', 'X', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'O', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'X', 'X', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'X', 'X', 'O', 'O', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'X', 'X', 'X', 'X', 'X', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], 
+            ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+        ]
+        self.get_path((1,1),(8,7))
         self.robot_state = {}
         self.print('num_robots: {}'.format(self.num_robots))
         self.print('shape: {}'.format(self.shape))
@@ -41,7 +65,6 @@ class Simple(Strategy):
         # Loop over robots
         for robot_id in range(self.num_robots):
             action.detect(robot_id)
-
             if not self.robot_state[robot_id]['directions_scanned']:
                 self.scan(robot_id, observation, 'right')
                 self.robot_state[robot_id]['directions_scanned'] = 'right'
@@ -54,8 +77,6 @@ class Simple(Strategy):
             elif self.robot_state[robot_id]['directions_scanned'] == 'left':
                 self.scan(robot_id, observation, 'up')
                 self.robot_state[robot_id]['directions_scanned'] = None
-
-            
         return action
 
     def scan(self, robot_id, observation, direction):
@@ -127,10 +148,79 @@ class Simple(Strategy):
                 for i in range(robot.position[1], bottom_y):
                     self.map[robot.position[0]][i] = 'O'
 
-        self.print(self.map)
 
+    def get_path(self, start, end):
+        a = self.map
+        for i in range(len(a)):
+            for j in range(len(a[i])):
+                if a[i][j] == 'X':
+                    a[i][j] = 1
+                else:
+                    a[i][j] = 0
 
+        m = []
+        for i in range(len(a)):
+            m.append([])
+            for j in range(len(a[i])):
+                m[-1].append(0)
+        i,j = start
+        m[i][j] = 1
 
+        k = 0
+        while m[end[0]][end[1]] == 0:
+            k += 1
+            for i in range(len(m)):
+                for j in range(len(m[i])):
+                    if m[i][j] == k:
+                        if i>0 and m[i-1][j] == 0 and a[i-1][j] == 0:
+                            m[i-1][j] = k + 1
+                        if j>0 and m[i][j-1] == 0 and a[i][j-1] == 0:
+                            m[i][j-1] = k + 1
+                        if i<len(m)-1 and m[i+1][j] == 0 and a[i+1][j] == 0:
+                            m[i+1][j] = k + 1
+                        if j<len(m[i])-1 and m[i][j+1] == 0 and a[i][j+1] == 0:
+                            m[i][j+1] = k + 1
+        
+        i, j = end
+        k = m[i][j]
+        path = [(i,j)]
+        while k > 1:
+            if i > 0 and m[i - 1][j] == k-1:
+                i, j = i-1, j
+                path.append((i, j))
+                k-=1
+            elif j > 0 and m[i][j - 1] == k-1:
+                i, j = i, j-1
+                path.append((i, j))
+                k-=1
+            elif i < len(m) - 1 and m[i + 1][j] == k-1:
+                i, j = i+1, j
+                path.append((i, j))
+                k-=1
+            elif j < len(m[i]) - 1 and m[i][j + 1] == k-1:
+                i, j = i, j+1
+                path.append((i, j))
+                k -= 1
+
+        path.reverse()
+        self.print(path)
+
+        # Get Moves
+        moves = []
+        for i in range(len(path)-1):
+            self.print(path[i],path[i+1])
+            if path[i+1][0] > path[i][0]: # down
+                moves.append('down')
+            elif path[i+1][0] < path[i][0]: # up
+                moves.append('up')
+            elif path[i+1][1] > path[i][1]: # right
+                moves.append('right')
+            elif path[i+1][1] < path[i][1]: # left
+                moves.append('left')
+
+        self.print(moves)
+
+        
 # Run strategy
 if __name__ == "__main__":
     strategy = Simple()
